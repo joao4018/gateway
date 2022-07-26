7bfaa6b1bbf74f79e91cc19a4007f936e857fdc0
joao4018 <joao4018@gmail.com>
2022-07-26 14:53:38 -0300

    Fix schemes swagger

--- src/main/java/base/joao4018/gateway/GatewayApplication.java
+++ src/main/java/base/joao4018/gateway/GatewayApplication.java
@@ -4,42 +4,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class GatewayApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Autowired
-    RouteDefinitionLocator locator;
-
-    @Bean
-    public List<GroupedOpenApi> apis() {
-        List<GroupedOpenApi> groups = new ArrayList<>();
-        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-        assert definitions != null;
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-            String name = routeDefinition.getId().replaceAll("-service", "");
-            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-        });
-        return groups;
     }
 
 }
