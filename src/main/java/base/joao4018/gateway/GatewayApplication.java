5d36e33471e6a7d7ea6dc2ab4b590c25843fee8c
joao4018 <joao4018@gmail.com>
2022-07-26 08:51:05 -0300

    add dev enviroment

--- src/main/java/base/joao4018/gateway/GatewayApplication.java
+++ src/main/java/base/joao4018/gateway/GatewayApplication.java
@@ -2,20 +2,20 @@ package base.joao4018.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.GroupedOpenApi;
+//import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GatewayApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
@@ -24,19 +24,19 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Autowired
-    RouteDefinitionLocator locator;
+//    @Autowired
+//    RouteDefinitionLocator locator;
 
-    @Bean
-    public List<GroupedOpenApi> apis() {
-        List<GroupedOpenApi> groups = new ArrayList<>();
-        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-        assert definitions != null;
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-            String name = routeDefinition.getId().replaceAll("-service", "");
-            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-        });
-        return groups;
-    }
+//    @Bean
+//    public List<GroupedOpenApi> apis() {
+//        List<GroupedOpenApi> groups = new ArrayList<>();
+//        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+//        assert definitions != null;
+//        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+//            String name = routeDefinition.getId().replaceAll("-service", "");
+//            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+//        });
+//        return groups;
+//    }
 
 }
