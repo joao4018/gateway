3072195cdd9f48997a1027171f05ec6767f84d9a
joao4018 <joao4018@gmail.com>
2022-07-26 14:51:01 -0300

    Fix schemes swagger

---
+++ src/main/java/base/joao4018/gateway/docs/OpenAPIConfiguration.java
@@ -0,0 +1,23 @@
+package base.joao4018.gateway.docs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.Collections;
+
+/**
+ * @author joao4018
+ */
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
+    }
+}
+}
